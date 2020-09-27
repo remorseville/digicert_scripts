6967bb1c0264f3d9e3bd6d311999c9babb268ed0
Ben Morse <morsewb@gmail.com>
2020-09-27 13:40:56 -0600

    Update user_access.py

--- user_access.py
+++ user_access.py
@@ -1,36 +1,47 @@
 import requests  
 import json  
 
-# --Variables--  
-old_user_id = 																						                                    # User ID of the "limited" user that controls the orders. No quotes.  
-api_key = "" 																						                                      # API key  
-user_id = 																							                                      # User ID of the "limited" user you want to give access to. No quotes.  
+# --Variables-- #
+
+# User ID of the "limited" user that controls the orders. No quotes. 
+old_user_id =
+
+# API key
+api_key = "" 
+
+# User ID of the "limited" user you want to give access to. No quotes.  
+user_id = 																							                                      
 
 
-headers = { 																						                                      # usual header info for call to DigiCert  
+
+
+headers = { 																						                                        
   'X-DC-DEVKEY': f'{api_key}',  
   'Content-Type': "application/json"  
   }  
 
-url = f'https://www.digicert.com/services/v2/order/certificate?user_id%5D={old_user_id}' 			# URL of the current users orders (old_user_id)   
-response = requests.request("GET", url, headers=headers) 											                # This response is grabbing all the orders for the URL variable specified on line 6  
+
+# URL of the current users orders (old_user_id) / Filtering options could be added here
+url = f'https://www.digicert.com/services/v2/order/certificate?user_id%5D={old_user_id}' 			  
+response = requests.request("GET", url, headers=headers) 											                 
 data = response.text  
 json_data = json.loads(data)  
 
 
-
-for orders in json_data["orders"]: 																	                          # This "for" loop is grabbing every order number in the provided data.  
+# This loop is grabbing every order number in the provided data.
+# A new URL and body for our request are created.
+for orders in json_data["orders"]: 		
+  
   order_id = orders["id"]  
-  url = f'https://www.digicert.com/services/v2/order/certificate/{order_id}/user-access' 			# A new URL is created for each order. This is the endpoint for managing user access.  
-  print(order_id, " ", url) 			# Some output of each order and it's URL.  
-  payload = f'{{"user_id_assignments":["{user_id}"]}}' 												                # This is body of the PUT request. It includes the "user_id" of the limited user you are looking to add. 
+  url = f'https://www.digicert.com/services/v2/order/certificate/{order_id}/user-access'
+  
+  # Some output of each order and it's URL.
+  print(order_id, " ", url) 			  
+  payload = f'{{"user_id_assignments":["{user_id}"]}}' 												                
   
   
-  
-   ## If you would like to see what orders are being referenced before commiting to any changes, comment out lines 33 and 34.
-   
-   
-  response = requests.request("PUT", url, data=payload, headers=headers) 							        # New response (PUT request) to DigiCert that sets our user  
+  # If you would like to see what orders are being referenced before commiting to any changes, comment out the lines below.
+  response = requests.request("PUT", url, data=payload, headers=headers) 							         
   print(response.text)
   
  
